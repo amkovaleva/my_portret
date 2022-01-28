28bf5558da026295a63e35b2f87dfa9852b82c42
alinastart <alinastart@mail.ru>
2022-01-28 12:58:27 +0300

    En version

---
+++ messages/en-EN/app/orders.php
@@ -0,0 +1,42 @@
+<?php
+return [
+    'title' => 'Make an order',
+    'portrait_type' => 'Portrait type',
+    'material' => 'Material',
+    'base' => 'Base',
+    'format' => 'Size',
+    'faces_count' => 'Number of persons',
+    'frame_format' => 'Frame size',
+    'no_frame' => 'No frame',
+    'frame' => 'Frame',
+    'frame_colour' => 'Frame color',
+    'mount' => 'Mat color',
+    'background_color' => 'Background color',
+    'cost' => 'Price',
+    'currency' => 'Currency',
+    'img_alt' => 'Portrait preview',
+    'crop_alt' => 'Selection of the portrait area.',
+
+    'saved' => 'The order has been successfully added to the cart.',
+    'not_saved' => 'An error occurred while adding an order to the cart.',
+
+
+    'ru' => '₽',
+    'en' => '$',
+    'eur' => '€',
+    'price_ru' => '{0, number}₽',
+    'price_en' => '${0, number}',
+    'price_eur' => '{0, number}€',
+
+    'faces_to' => 'up to {0, number} persons',
+    'to_cart' => 'Add to Cart',
+    'miss_image' => 'Upload an image',
+
+
+    'clear' => 'Clear',
+    'change_orientation' => 'Change orientation',
+    'change_area' => 'Change photo area',
+
+    'cropper_title' => 'Choose a photo area',
+    'cropper_apply' => 'Apply',
+];
